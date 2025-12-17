655de20474d2bfd4f00e44fed4d38974e01185c8
MAJDA <majdaerrazani3@gmail.com>
2025-12-17 17:15:55 +0100

    new

---
+++ projet2/auth_service/src/main/java/com/example/auth_service/repository/UtilisateurRepository.java
@@ -0,0 +1,12 @@
+package com.example.auth_service.repository;
+
+
+import com.example.auth_service.model.Utilisateur;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
+    Optional<Utilisateur> findByUsername(String username);
+    boolean existsByUsername(String username);
+    Optional<Utilisateur> findByActivationToken(String resetToken);
+}
