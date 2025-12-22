fadfc96709b53c82fc15d4a9740c01db41fd174d
Assiaamahouch1 <assia.amahouch@usms.ac.ma>
2025-12-22 19:46:37 +0100

    consultation service repositories

--- projet/consultation-service/src/main/java/com/yourorg/healthcare/consultationservice/repository/ConsultationRepository.java
+++ projet/consultation-service/src/main/java/com/yourorg/healthcare/consultationservice/repository/ConsultationRepository.java
@@ -1,13 +1,13 @@
 package com.yourorg.healthcare.consultationservice.repository;
+
 import com.yourorg.healthcare.consultationservice.model.Consultation;
-import org.springframework.data.domain.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.Instant;
 import java.util.UUID;
 
 public interface ConsultationRepository extends JpaRepository<Consultation, UUID> {
     Page<Consultation> findByPatientId(UUID patientId, Pageable pageable);
-    Page<Consultation> findByDoctorId(UUID doctorId, Pageable pageable);
-    Page<Consultation> findByDateBetween(Instant start, Instant end, Pageable pageable);
+    Page<Consultation> findByMedecinId(UUID medecinId, Pageable pageable);
 }
