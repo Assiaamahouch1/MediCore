3aff8fcf3e7ac47193458f896829d2f86e7dbc39
MAJDA-ER-RAZANI <159846776+MAJDA-ER-RAZANI@users.noreply.github.com>
2025-12-24 18:20:11 +0100

    Delete IdeaProjects directory

--- IdeaProjects/projet/cabinet-service/src/main/java/com/example/cabinetservice/repository/CabinetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.cabinetservice.repository;
-
-import com.example.cabinetservice.model.Cabinet;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CabinetRepository extends JpaRepository<Cabinet, Long> {
-}
